a208fc5919afb402b66bc5917429d68d6c728a09
HazzaBui <harrystevenson@hotmail.com>
2014-01-24 23:02:42 +0000

    Added missing files + level manager

--- ggj14/ggj14/Program.cs
+++ ggj14/ggj14/Program.cs
@@ -10,7 +10,7 @@ namespace ggj14
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (levelManager game = new levelManager())
             {
                 game.Run();
                 int i = 0;
---
+++ ggj14/ggj14/levelManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ggj14
+{
+    public class levelManager : Microsoft.Xna.Framework.Game
+    {
+        public levelManager()
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+    }
+}
