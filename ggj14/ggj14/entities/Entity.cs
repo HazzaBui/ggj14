90a26ea6e515229308f9cec6c03418586f723840
Link345 <Link345@gmail.com>
2014-01-24 23:59:53 +0000

    Entity class update

--- ggj14/ggj14/entities/Entity.cs
+++ ggj14/ggj14/entities/Entity.cs
@@ -12,7 +12,67 @@ namespace ggj14.entities
         protected Texture2D texture;
         protected Vector2 position;
         protected Vector2 velocity;
+        protected Vector2 centre;
         protected bool facingLeft;
 
+
+        public Vector2 getVelocity()
+        {
+            return velocity;
+        }
+
+        public Vector2 getPosition()
+        {
+            return position;
+        }
+
+        public Texture2D getTexture()
+        {
+            return texture;
+        }
+
+        public Vector2 getCentre()
+        {
+            return centre;
+        }
+
+        public void setVelocity(Vector2 inVel)
+        {
+            velocity = inVel;
+        }
+
+        public void setPosition(Vector2 inPos)
+        {
+            position = inPos;
+        }
+
+        public void setTexture(Texture2D inTex)
+        {
+            texture = inTex;
+        }
+
+        public void setCentre(Vector2 inCentre)
+        {
+            centre = inCentre;
+        }
+
+        public Entity(Texture2D inTex, Vector2 inPos)
+        {
+            velocity = Vector2.Zero;
+            facingLeft = false;
+            texture = inTex;
+            position = inPos;
+            centre = new Vector2((position.X + texture.Width) / 2, (position.Y + texture.Height) / 2);
+        }
+
+        public virtual void Update()
+        {
+            this.centre = new Vector2((position.X + texture.Width) / 2, (position.Y + texture.Height) / 2);
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, centre, null, Color.White);
+        }
     }
 }
