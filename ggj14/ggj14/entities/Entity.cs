ca761a2aa5929d5022b1e0853b66e83e9e5eddd2
Link345 <Link345@gmail.com>
2014-01-24 23:43:41 +0000

    Added barebones Entity Class

---
+++ ggj14/ggj14/entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ggj14.entities
+{
+    public class Entity
+    {
+        protected Texture2D texture;
+        protected Vector2 position;
+        protected Vector2 velocity;
+        protected bool facingLeft;
+
+    }
+}
